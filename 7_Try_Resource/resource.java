019491fdb0a0a1929daf6a7678e393abeb17b8d6
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-11-30 14:38:57 +0530

    Throws

---
+++ 7_Try_Resource/resource.java
@@ -0,0 +1,15 @@
+public class resource {
+    public static void main(String[] args) {
+        // no compulsion try nd catch
+        // we can try and finally 
+       int i=0;
+       int j = 0;
+
+        try{
+            j = 18/i;
+        }
+        finally{
+            System.out.println("Bye..!");
+        }
+    }
+}
